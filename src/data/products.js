7cd3be4297f8f14301d321c7fbe60ca3dd18a050
Njabs-3550 <magcaban115@gmail.com>
2026-05-09 22:04:17 +0200

    Fix cover image file extension for product

--- src/data/products.js
+++ src/data/products.js
@@ -27,7 +27,7 @@ const products = [
     subcategory: "polos",
     price: 899,
     description: "Loose saggy fit. Extended sleeves. Soft structured collar. Slight drop hem.",
-    coverImage: "/images/products/green&white-golfer.pn",
+    coverImage: "/images/products/green&white-golfer.png",
     sizes: ["S", "M", "L", "XL"],
     colors: [
       { name: "Navy", hex: "#1B2838", image: "/images/products/bgreen-golfer.png" },
